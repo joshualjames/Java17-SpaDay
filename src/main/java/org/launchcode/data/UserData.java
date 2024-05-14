01e6b0793dbd48574cf5e280a13e47b89b75a343
Joshua James <josh452063@gmail.com>
2024-05-13 19:50:41 -0500

    first part

---
+++ src/main/java/org/launchcode/data/UserData.java
@@ -0,0 +1,20 @@
+package org.launchcode.data;
+
+import org.launchcode.models.User;
+
+import java.util.ArrayList;
+
+public class UserData {
+    public User addUser(User user){
+        return user;
+    }
+
+    public ArrayList<User> getAllUsers(){
+//        ArrayList<User> users =
+        return getAllUsers();
+    }
+
+    public User getUserById(){
+        return getUserById();
+    }
+}
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,9 +1,20 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message = "Username is required")
+    @Size(min=5, max=5, message = "Username must be between 5 and 15 characters")
     private String username;
+    @Email(message = "Email must be a valid email address")
     private String email;
+    @NotBlank(message = "Password is required")
+    @Min(value = 6, message = "Password must be at least 6 characters long")
     private String password;
+    private int id;
 
     public User() {
 
@@ -14,6 +25,14 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getUsername() {
